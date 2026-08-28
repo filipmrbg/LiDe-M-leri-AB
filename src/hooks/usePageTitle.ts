7c778f7379e143082067415463cdd53ca27ebeef
Filip <f.bjorgaas@gmail.com>
2026-08-28 14:52:14 +0200

    hej

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -41,7 +41,7 @@ export function usePageTitle(title: string, description?: string) {
       ? window.location.origin
       : 'https://lidemaleri.se';
     const absoluteUrl = `${origin}${pathname === '/' ? '' : pathname}`;
-    const ogImageUrl = 'https://media.fastdl.app/get?__sig=60gyNjdptzxMJ7FochySEQ&__expires=1787913596&uri=https%3A%2F%2Finstagram.fdac24-5.fna.fbcdn.net%2Fv%2Ft51.2885-19%2F180430162_359569148844349_1575881971785143854_n.jpg%3Fstp%3Ddst-jpg_s150x150_tt6%26_nc_cat%3D102%26ccb%3D7-5%26_nc_sid%3Df7ccc5%26efg%3DeyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%253D%26_nc_ohc%3DGfAFGHybTo0Q7kNvwFL1nr2%26_nc_oc%3DAdpHcpiUU8sgSVivfT_9Lk0qHDRPZ8V-uZ1E-UINhhGU3iNP1lZDiqoMV05JdDmD5Ps%26_nc_zt%3D24%26_nc_ht%3Dinstagram.fdac24-5.fna%26_nc_ss%3D7baaf%26oh%3D00_AQFkzo1ZaG-w8RaNBLDKXpprUwy1sBOgDvxPRnjNaasniA%26oe%3D6A972C00&filename=180430162_359569148844349_1575881971785143854_n.jpg';
+    const ogImageUrl = `${origin}/og-image.png`;
 
     let canonical = document.querySelector('link[rel="canonical"]');
     if (canonical) {
